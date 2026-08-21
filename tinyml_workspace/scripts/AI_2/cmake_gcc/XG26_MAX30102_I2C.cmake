e3a91c2bd8704d207695b416ff018c210b6ba23f
QUACH TUAN <tuanyqa@gmail.com>
2026-08-21 16:24:27 +0700

    Add files via upload

---
+++ tinyml_workspace/scripts/AI_2/cmake_gcc/XG26_MAX30102_I2C.cmake
@@ -0,0 +1,30 @@
+####################################################################
+# Automatically-generated file. Do not edit!                       #
+####################################################################
+
+set(SDK_PATH "C:/Users/Asus/.silabs/slt/installs/conan/p/simpl508ee6c1a6569/p")
+set(COPIED_SDK_PATH "")
+set(PKG_PATH "C:/Users/Asus/.silabs/slt/installs")
+
+add_library(slc OBJECT
+)
+
+target_include_directories(slc PUBLIC
+)
+
+target_compile_definitions(slc PUBLIC
+)
+
+target_link_libraries(slc PUBLIC
+    "-Wl,--start-group"
+    "-Wl,--end-group"
+)
+target_compile_options(slc PUBLIC
+)
+
+set(post_build_command )
+
+target_link_options(slc INTERFACE
+)
+
+# BEGIN_SIMPLICITY_STUDIO_METADATA=eJztVm1P2zAQ/iuRP07UDikUGhUmVMrUiapVX6ZNBEWO4xavSRzZTtmE+t93ThoKFKZN04Y00aquc7l7fPfcJXd3aNIfjC773f70SziZzs77w3B0PpggH3Xef0uTIHBWXGkhs5MA7WM3QCDhGZOxyBYgmk0vGscBen8aBIGCX9bJlfzKmQG1jKYcVAqGUxkXCceamyLHuciMlAn+/MFrhYOzz0133/XCvtfFC8ZKfIDIuTLfJwz+AaGGROT+GOdveUdzypYhFWmC01XeYDLNRcLVn3mL9tBoPPzY607D8XA4BXLvAjTuXZ5N+5964cNbAfIDhHGA1mCzm5rB8Hx22bPZuQIExVO54jHYzGmi+V6AokIkRmS9bywpYq7hztX1VjyRhWJbaRV3eWLNiz8YlEIHyM20v5GeQAzoxpjcJ+T29hZrkdBIY+CGaE1GlRLmTCpuNZ3NcaWZUUUlFHF5/XvVYA3zOH2EdFqSmjlOR2QQps2+dnJqDFfVkfidXclGr0OexHYaoC0lEL3FXe/9J3T+Xvn+M3qvoZrh8RA8vgB3tK3f62cLHJ6FUW887Zc1DjnZ8bvk9856oCkka2IkW36iStAIcK3Yt4tVsB8kMm1oklxKRg1EUov9ejMHd3zS9clMw4uEnOlCk01CiE4M2dhrQhW7EStOVq1GRDVvrFrYw27DO/bIRKR5IpiAV4ApYiEbLaKNDiObCZ7FpD5rr97ky0W/wh1Rc/OCT+RXvNqB/s6p2gH0XK+1oxlLpl+VFnBgx6kNLpz5qq5t3ahPXVeLLe1zPqdFYt4K8a0QX7EQ1w9mislwNu727me2pzPR8xOR2k5DL48wpRq0AiazuVgUqmSiHp3uHwQHYubJI0kkqYq70H3AIhIJBAi3IxV7R26b+i623zO3afsPVeapYsoKTFWK+Vw1PZwuvFa1tbvocN+dNz3XFekBROLY6YHdUJE9BYHeV7dWrfFcgdO3Ui3xJjhs+yvO5BTsu9a+8goQdbz8GVa8xJAUBquAvW9rGrfANGwfHnpH7bbb9ppHBwfeYbvdumcQOIQBgimRWwZPO+Th1YZk8ojlKkN1dy2v0PoHYEsD5g===END_SIMPLICITY_STUDIO_METADATA
